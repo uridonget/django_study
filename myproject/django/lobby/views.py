e1a457277f90c2264ad75666c1be3bac93a5b4e0
uridonget <100407513+uridonget@users.noreply.github.com>
2024-10-28 19:20:11 +0900

    project structure fixed(static/app/js)

--- myproject/django/lobby/views.py
+++ myproject/django/lobby/views.py
@@ -3,6 +3,3 @@ from django.core.exceptions import PermissionDenied
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_simplejwt.tokens import AccessToken
 from django.http import JsonResponse, HttpResponse
-
-def index(request):
-    return render(request, 'lobby/index.html')
